a54094b3a083cfc4e098970b3b719e73a1fd9699
Eli Thomas <38369558+eliThomass@users.noreply.github.com>
2024-12-27 21:14:18 -0800

    added timestamp feature, the program will now return timestamps of topics talked about, this feature is inconsistent however; ChatGPT has trouble getting the times right.

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import summary
 video_url = input('Enter video URL: ')
 video_id = video_url.replace('https://www.youtube.com/watch?v=', '')
 
-print('--- SUMMARY --- \n')
+print('\n--- SUMMARY --- \n')
 print(summary.getSummary(video_id))
+print('\n')
+print('--- TIMESTAMPS --- \n')
+print(summary.getTimestamps(video_id))
 print('\n')
--- summary.py
+++ summary.py
@@ -2,12 +2,12 @@ from youtube_transcript_api import YouTubeTranscriptApi
 from openai import OpenAI
 
 # Enter your OWN OpenAI API Key in the string below
-client = OpenAI(api_key = '')
+client = OpenAI(api_key = ' ENTER KEY HERE ')
 
 
 def getTranscript(video_id):
-    json_transcript = YouTubeTranscriptApi.get_transcript(video_id)
     transcript = ''
+    json_transcript = YouTubeTranscriptApi.get_transcript(video_id)
     counter = 0
     for x in json_transcript:
         sentence = x['text']
@@ -25,7 +25,23 @@ def getSummary(video_id):
         model = "gpt-3.5-turbo",
         messages =   [
             {"role": "system", "content": "You are a journalist."},
-            {"role": "assistant", "content": "Write a 100 word summary of this video"},
+            {"role": "assistant", "content": "What topics are discussed in the video? Provide start time codes in seconds" },
+            {"role": "user", "content": transcript}
+        ]
+    )
+
+    summary = response.choices[0].message.content
+
+    return summary
+
+def getTimestamps(video_id):
+    transcript = getTranscript(video_id)
+
+    response = client.chat.completions.create(
+        model = "gpt-3.5-turbo",
+        messages =   [
+            {"role": "system", "content": "You are a database organizer."},
+            {"role": "assistant", "content": "Give timestamps of things talked about in the transcript, the timestamps should be in MM:SS format"},
             {"role": "user", "content": transcript}
         ]
     )
