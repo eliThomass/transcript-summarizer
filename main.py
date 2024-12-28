0f757005f15d38b152237930d632f1ccc4282e43
Eli Thomas <38369558+eliThomass@users.noreply.github.com>
2024-12-27 19:16:59 -0800

    implemented getTranscript function, we can now paste a URL into main.py and get the full JSON transcript back

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-from youtube_transcript_api import YouTubeTranscriptApi
+import summary
 
-print("hi")
+video_url = input("Enter video URL: ")
+video_id = video_url.replace("https://www.youtube.com/watch?v=", "")
+
+print(summary.getTranscript(video_id))
+
--- summary.py
+++ summary.py
@@ -1,4 +1,6 @@
-from youtube_transcript_api import YouTubeTranscriptAPI
+from youtube_transcript_api import YouTubeTranscriptApi
+import openai
 
-def getTranscript():
-    
+def getTranscript(video_id):
+    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    return transcript
